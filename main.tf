e0dbd6cdfd404ef601d7a696dbcfe478e378ff8b
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-09-27 20:59:29 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+locals{ 
+  storage_name= ["north","south","east","west"]
+  superstar= ["jade","saj","idk","douglas","emannuel","olarewaju"]
+
+}
+  resource "azurerm_resource_group" "butterfly" {
+  name     = "lmao"
+  location = "Central Canada"
+}
+
+resource "azurerm_storage_account" "azurestorage" {
+  name                     = "azurestor"
+  resource_group_name      = azurerm_resource_group.butterfly.name
+  location                 = azurerm_resource_group.butterfly.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "azurecontainer" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.azurestorage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "awesomecontent" {
+  for_each            = { for blob in local.storage_name : blob => blob }
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.azurestorage.name
+  storage_container_name = azurerm_storage_container.azurecontainer.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
+}
+
